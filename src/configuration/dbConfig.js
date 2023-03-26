ba19dbe228e091126ad1e6dd146d99594dc86c79
Discut <siiiro@outlook.com>
2023-03-26 22:19:52 +0800

    feat(Config): 🎨 新增系统配置文件

--- src/configuration/dbConfig.js
+++ src/configuration/dbConfig.js
@@ -1,4 +1,9 @@
 module.exports = {
     DATABASE_NAME: 'comic.db',
-    COMIC_COLLECTION_TABLE_NAME: 'comic_collection'
+    DOWNLOAD_DATABASE_NAME: 'download.db',
+    ACCOUNT_DATABASE_NAME: 'account.db',
+    COMIC_DOWNLOAD_TABLE_NAME: 'download',
+    ACCOUNT_TABLE_NAME: 'account',
+    COMIC_COLLECTION_TABLE_NAME: 'comic_collection',
+
 }
---
+++ src/configuration/systemConfig.js
@@ -0,0 +1,4 @@
+const path = require('path');
+module.exports = {
+    DOWNLOAD_DIR: path.join(process.cwd(), 'download')
+}
