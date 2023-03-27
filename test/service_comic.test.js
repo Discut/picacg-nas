0b79f8732628015828f5942bc8820fcf0b79cb61
Discut <siiiro@outlook.com>
2023-03-27 16:38:19 +0800

    test(ComicServiceTest): ✅ 新增 漫画信息与章节获取功能单元测试

--- test/service_comic.test.js
+++ test/service_comic.test.js
@@ -3,15 +3,29 @@ const ComicCollectionDao = require("../src/dao/comicCollection");
 const ComicService = require("../src/service/comicService");
 
 describe('ComicService', function () {
-    describe('测试保存图片', () => {
+    const dao = new ComicCollectionDao();
+    const service = new ComicService();
+    describe.skip('测试保存图片', () => {
         it('保存封面', () => {
-            const dao = new ComicCollectionDao();
             dao.queryAll().then(comics => {
                 new ComicService()._updateComicFile([comics[0]]).then(response => {
 
                 }).catch(err => {
                     console.error(err)
                 })
+            }).catch(err => {
+                console.error(err)
+            })
+        })
+
+    });
+
+    describe('测试获取漫画章节', function() {
+        this.timeout(5000)
+        it('开始获取', (done) => {
+            dao.queryAll().then(async comics => {
+                await service.synchronousEpisodeInfo([comics[7]]);
+                done();
             }).catch(err => {
                 console.error(err)
             })
